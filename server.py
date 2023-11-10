0434e8e8b14fcda2616f98d514f83fec4c9bb720
mcgourthynatwit <100015002+mcgourthynatwit@users.noreply.github.com>
2023-11-10 11:59:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import threading
 import socket
-
+from threading import Lock
 host = '127.0.0.1' # local host
-port = 59000
+port = 12342
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
@@ -18,45 +18,58 @@ class Client:
         self.name = name
 
     def send_message(self, message):
-        self.connection.send(message.encode('utf-8'))
+        self.connection.send(bytes(str(message), 'utf-8'))
 
     def recieve_message(self):
         return self.connection.recv(1024).decode('utf-8')
 
+global riders, drivers, requested
+
 riders = [] # holds riders waiting for a ride
+riders_lock = Lock()
 drivers = [] # holds drivers waiting to take a ride
+drivers_lock = Lock()
 requested = {} # dictionary that holds requested riders and they're requested ride distance
+requested_lock = Lock()
 
 def get_riders():
-    for rider in riders:
-        print(rider)
+    with riders_lock:
+        for rider in riders:
+            print(rider)
 
 def find_rider(name):
-    for rider in riders:
-        if rider.name == name:
-            return rider
+    with riders_lock:
+        for rider in riders:
+            if rider.name == name:
+                return rider
     print('Rider not found')
 
 def remove_rider(name):
-    for rider in riders:
-        if rider.name == name:
-            riders.remove(rider)
-def get_drivers():
+    with riders_lock:
+        for rider in riders:
+            if rider.name == name:
+                riders.remove(rider)
+
+def send_message_to_driver(message):
     for driver in drivers:
-        print(driver)
+        print(driver.name)
+        print(driver.connection)
+        driver.send_message(message)
         
 def find_driver(name):
-    for driver in drivers:
-        if driver.name == name:
-            return driver
+    with drivers_lock:
+        for driver in drivers:
+            if driver.name == name:
+                return driver
     print('driver not found')
 
 def remove_driver(name):
-    for driver in drivers:
-        if driver.name == name:
-            drivers.remove(driver)
+    with drivers_lock:
+        for driver in drivers:
+            if driver.name == name:
+                drivers.remove(driver)
 # TODO:
-def connect_clients(driver, rider):
+# def connect_clients(driver, rider):
     # Get IP of driver & rider
 
     # Exchange IP & Port to each
@@ -67,56 +80,77 @@ def connect_clients(driver, rider):
 
 def handle_rider(rider):
     # Prompt rider for their destination(length)
+    rider.send_message('What is the distance in miles for your trip? (In miles)')
+    distance = rider.recieve_message()
     # Prompt rider for how long this should take
+    rider.send_message('How long do you think this will take (HH:MM)')
+    eta = rider.recieve_message()
+
     # Send message to all drivers, maybe push this to a requested array so drivers who join after can see this 
+    send_message_to_driver((rider.name + 'has requested a ride of '+ distance + ' miles and should take' + eta + 'minutes.'))
     # Wait 30 seconds or something if no response send message
     # If a driver wants to take it run connect_clients() for them to enter a chatroom and negotiatie
 
 def handle_driver(driver):
+    driver.send_message(('Hello driver ' + driver.name))
     # Recieve requests from riders
     # Get to choose if they want to take a drive of the distance
     # If yes run connect_clients()
 
-def send_to_drivers(requested):
+# def send_to_drivers(requested):
     # Iterates over requested dictionary
     # sends a formatted message with the current requests allowing drivers to choose
 
-def main():
+def main(client_obj):
     while True:
-        print('Server running...')
-        client, address = server.accept()
+        client_obj.send_message('What is your name')
+        client_obj.name = client_obj.recieve_message()
 
-        client_obj = Client(client_socket, address)
-        print('Connection established with ', client_obj.address)
-        
-        while True:
-            client_obj.send('Are you a driver or a rider?'.encode('utf-8'))
-            client_role = client_obj.recieve_message()
-
-            if client_role.lower() == 'driver':
-                print('Client is choosing to be a driver')
-                drivers.append(client_obj)
-                break
-
-            elif client_role.lower() == 'rider':
-                print('Client is choosing to be a rider')
-                riders.append(client_obj)
-                break
-
-            else:
-                print('Invalid choice')
-                client_obj.send_message('Invalid choice please enter driver or rider')
+        client_obj.send_message('Are you a driver or a rider?')
+        client_role = client_obj.recieve_message()
 
         if client_role.lower() == 'driver':
-            thread = threading.Thread(target = handle_driver, args=(client_obj,))
-            thread.start()
+            print('Client is choosing to be a driver')
+            with drivers_lock:
+                drivers.append(client_obj)
+                print('driver added')
+            break
 
-        if client_role.lower() == 'rider':
-            thread = threading.Thread(target = handle_rider, args=(client_obj,))
-            thread.start()
+        elif client_role.lower() == 'rider':
+            print('Client is choosing to be a rider')
+            with riders_lock:
+                riders.append(client_obj)
+                print('rider added')
+            break
 
-    # client connects to server
 
-    # server sends messages asking client if they're a driver/rider, then their name
+        else:
+            print('Invalid choice')
+            client_obj.send_message('Invalid choice please enter driver or rider')
 
-    # server 
+    if client_role.lower() == 'driver':
+        handle_driver(client_obj)
+
+    if client_role.lower() == 'rider':
+        handle_rider(client_obj)
+
+# client connects to server
+
+# server sends messages asking client if they're a driver/rider, then their name
+
+# server 
+
+while True:
+    print('Server running...')
+    client, address = server.accept()
+
+    client_obj = Client(client, address)
+    print('Connection established with ', client_obj.address)
+
+    thread = threading.Thread(target = main, args = (client_obj,))
+    thread.start()
+
+    shutdown = input('shutdown server y/n')
+    if shutdown == 'y':
+        server.close()
+ 
