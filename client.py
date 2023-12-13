2a7e302885924fb12e4abbad09fb6aa9782aefc6
mcgourthynatwit <100015002+mcgourthynatwit@users.noreply.github.com>
2023-12-12 22:58:38 -0500

    finish client.py

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ import socket
 import threading
 import random
 import time
+import sys
+
 class Client:
     # Contructor to create client variable
     def __init__(self, connection, address):
@@ -21,6 +23,12 @@ class Client:
     def recieve_message(self):
         return self.connection.recv(1024).decode('utf-8')
 
+# main server connection
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Port = 3007
+Host = '127.0.0.1'
+server.connect((Host, Port))
+
 # This will control the thread to send messages to main server, turned off when clients join chatroom
 main_server_connection = True
 # This will control if the clients come to an agreement on a price and closes the connection with main server
@@ -30,9 +38,9 @@ driver = False
 # Handles sending messages, only will become false when a chatroom agreement
 running = True
 
-# send messages to main server or chatroom 
+# send messages to main server or chatroom, thread running to be able to freely send responses as messages come in
 def send_messages():
-    global main_server_connection, server, chatroom_server, running
+    global main_server_connection, server, chatroom_server, running, agreed
     while running:
         try:
             if not driver:
@@ -47,18 +55,23 @@ def send_messages():
                 '''
                 Because of how we handle sending messages through a separate thread, when a chatroom starts the driver is required to input twice 
                 the first input here on line 37 then the thread understands that the driver is in fact a driver(line 36) the program then reaches 
-                the inputs inside negotiate(). Since input is a blocking method there was no way around this, to fix that we added a 1/10 s wait time
-                so the client can recieve aknowledgement from the main server that a chatroom is being created and the state is fixed. 
+                the inputs inside negotiate(). Since input is a blocking method there was no way around this, to fix that we added a wait time
+                so the client can recieve aknowledgement from the main server that a chatroom is being created and the state is fixed. This was especially
+                needed when we where testing on multiple devices going over the internet as opposed to local host
                 '''
-                time.sleep(.1)
+                time.sleep(1)
         except Exception as e:
             break
 # create chatroom server used by drivers
-def create_chatroom(chat_port):
+def create_chatroom(chat_port, chat_ip):
     global main_server_connection, driver
-    host = '127.0.0.1' 
+    host = chat_ip
     chatroom_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     chatroom_server.bind((host,int(chat_port)))
+
+    # allow 10 seconds until server times out
+    chatroom_server.settimeout(10)
+
     # allow a single connection at a time
     chatroom_server.listen(1) 
 
@@ -67,25 +80,30 @@ def create_chatroom(chat_port):
     driver = True
 
     while True:
-        client, address = chatroom_server.accept()
-        client_obj = Client(client, address)
-        print('Client joined ' , client_obj)
-        # only want one client
-        break
+        try:
+            client, address = chatroom_server.accept()
+            client_obj = Client(client, address)
+            # only want one client
+            print('Rider joined chatroom waiting for message...')
+            break
+        except socket.timeout:
+            print('Error with client joining, returning to main lobby')
+            main_server_connection = True
+            driver = False
+            return
 
     negotiate(chatroom_server, client_obj)
 
-
-# join chatroom server used by riders 
+# join chatroom server used by riders given ip and port from driver
 def join_chatroom(IP, PORT):
     global main_server_connection, chatroom_server
     chatroom_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     chatroom_server.connect((IP, PORT))
-    print(f"Joined chatroom at {IP}:{PORT}")
+    print(f"Joined chatroom")
     main_server_connection = False
     return chatroom_server
 
-# leave chatroom server used by clients
+# leave chatroom server used by clients, either allows clients to return to main server or if they agreed on a price close connections
 def leave_chatroom(chatroom_server):
     global main_server_connection, agreed, driver, running
 
@@ -102,6 +120,7 @@ def leave_chatroom(chatroom_server):
         main_server_connection = True
         server.send(bytes(str('decline'), 'utf-8'))
 
+# cycles through open ports to find to create chatroom
 def get_open_port():
     # runs through random number 1024 - 65535 and checks if the port is in use
     not_valid = True
@@ -113,17 +132,15 @@ def get_open_port():
     
     return str(random_port)
 
+# negotiate price between driver and rider
 def negotiate(server, client):
     global agreed, driver_input, thread
     current_price = 0.0
-    # variable to ensure driver gets input on price as well, rider cant just accept a initial price of 0
+    # variable to ensure driver gets input on price as well, rider cannot just accept a initial price of 0
     turns = False
 
     while True:
-        client.send_message(f'The current price is {current_price}\n')
-        client.send_message('1. Enter a new price\n')
-        client.send_message('2. Agree on this price\n')
-        client.send_message('3. Leave chatroom and find new ride\n')
+        client.send_message(f'The current price is {current_price}\n1. Enter a new price\n2. Agree on this price\n3. Leave chatroom and find new ride\n')
         response = client.recieve_message()
         match response:
             case '1':
@@ -132,7 +149,7 @@ def negotiate(server, client):
                     response = client.recieve_message()
                     try:
                         if float(response) > 0:
-                            client.send_message('Price request sent, waiting for response from driver...')
+                            client.send_message('Price request sent, waiting for response from driver...\n')
                             current_price = float(response)
                             break
                         else:
@@ -156,10 +173,7 @@ def negotiate(server, client):
             case _:
                 client.send_message('Invalid input')
 
-        print(f'The client has chosen a price of {current_price}\n')
-        print('1. Enter a new price\n')
-        print('2. Agree on this price\n')
-        print('3. Close connection and find new rider\n')
+        print(f'The client has chosen a price of {current_price}\n1. Enter a new price\n2. Agree on this price\n3. Close connection and find new rider\n')
         
         driver_response = input()
 
@@ -170,7 +184,7 @@ def negotiate(server, client):
                         try:
                             if float(response) > 0:
                                 current_price = float(response)
-                                print('Price request sent, waiting for response from rider...')
+                                print('Price request sent, waiting for response from rider...\n')
                                 turns = True
                                 break
                             else:
@@ -190,16 +204,13 @@ def negotiate(server, client):
                     leave_chatroom(server)
                     break
 
-# main server connection
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-Port = 3005
-Host = socket.gethostname()
-server.connect((Host, Port))
-
 # Start a separate thread for sending messages
 thread = threading.Thread(target=send_messages)
+# daemon threads end when the main process ends without waiting for actual thread to finish 
+thread.daemon = True
 thread.start()
 
+# main loop running 
 while True:
     try:
         # Chatroom negotiation occured and clients came to an agreement, leave main server
@@ -217,15 +228,21 @@ while True:
                 # prompt for driver 
                 case "What port number do you wish to have your chatroom on?":
                     driver = True
+                    chatroom_ip = '127.0.0.1'
                     chatroom_port = get_open_port()
+                    server.send(bytes(chatroom_ip, 'utf-8'))
+                    time.sleep(.1)
                     server.send(bytes(chatroom_port, 'utf-8'))
-                    create_chatroom(chatroom_port)
+                    create_chatroom(chatroom_port, chatroom_ip)
                 # prompt for rider
                 case "Chatroom information:":
                     chatroom_ip = server.recv(1024).decode('utf-8')
                     chatroom_port = int(server.recv(1024).decode('utf-8'))
                     print('Joining chatroom...')
                     chatroom_server = join_chatroom(chatroom_ip, chatroom_port)
+                case "Server shutdown":
+                    print('Main server shutting down, goodbye...')
+                    break
                 case _:
                     # if no relating messages to chatroom, print out message on client side
                     print(message)
@@ -234,6 +251,7 @@ while True:
             message = msg.decode('utf-8')
             print(message)
             
+            # Specific messages from servers to handle specific actions 
             if message == 'Sorry you could not come to an agreement, returning to main lobby to find new driver.' or message == 'Driver is no longer interested returning you to main lobby.':
                 print('closing...')
                 chatroom_server.close()
@@ -248,4 +266,11 @@ while True:
         print("Connection was lost with the server.")
         break
 
+# close connection and end process
 server.close()
+sys.exit(0)
+
+
+if chatroom_server:
+    chatroom_server.close()
+# Thread will still run and await input so we just terminate program.
